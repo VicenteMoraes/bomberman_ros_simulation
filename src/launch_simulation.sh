52ea71e684f5bd9c6fee3387bc3282529a05fdd5
VicenteMoraes <vicenteromeiromoraes@gmail.com>
2022-05-06 11:47:13 -0300

    Replicable launch

--- src/launch_simulation.sh
+++ src/launch_simulation.sh
@@ -2,4 +2,5 @@
 
 morse import /bomberman_ws/bomberman
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/bomberman_ws/bomberman/
+chmod +x /bomberman_ws/bomberman/morse_2dnav/scripts/*
 morse run bomberman & roslaunch morse_2dnav nav.launch
